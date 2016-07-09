1d22e015472082f286fafa851c7b6677e11ea765
Colin Kingen <colinwkingen@gmail.com>
2016-07-09 15:18:16 -0700

    update README and continue work on app.rb routing problems

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require './lib/define'
 @@current_word = []
+@@word_index = 0
 get('/') do
   erb(:index)
 end
@@ -32,10 +33,21 @@ post('/defined_it') do
   erb(:word_addition)
 end
 post('/local_definition') do
-   @word_index = params.fetch('view_defs').to_i
-   erb(:this_definition)
+  definition = params.fetch('new_definition')
+  @@word_index = params.fetch('view_defs').to_i
+  erb(:this_definition)
 end
 get('/local_definition') do
-  @word_index = params.fetch('view_defs').to_i
+  @@word_index = params.fetch('view_defs').to_i
+  @@current_word[@@word_index][2] += definition
+  erb(:this_definition)
+end
+post('/new_definitions') do
+  definition = params.fetch('new_definition')
+  @@word_index = params.fetch('view_defs').to_i
+  erb(:this_definition)
+end
+get('/new_definitions') do
+  @@current_word[@@word_index][2] += definition
   erb(:this_definition)
 end
